2fd69584239e0318d000969571c1be86cbd12c47
cjhanc15 <102087890+cjhanc15@users.noreply.github.com>
2023-03-07 18:53:20 -0700

    Delete milestone1/doc directory

--- milestone1/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"milestone1","l":"InventoryManager"},{"p":"milestone1","l":"SalableProduct"},{"p":"milestone1","l":"ShoppingCart"},{"p":"milestone1","l":"StoreFront"}];updateSearchResults();
