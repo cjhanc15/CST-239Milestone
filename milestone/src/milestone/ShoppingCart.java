62c76b6f1a146e8bba39d30f9a83c31efdb6ca09
cjhanc15 <102087890+cjhanc15@users.noreply.github.com>
2023-03-11 17:34:15 -0700

    Add files via upload

---
+++ milestone/src/milestone/ShoppingCart.java
@@ -0,0 +1,72 @@
+package milestone;
+
+import java.util.ArrayList;
+
+public class ShoppingCart {
+    private ArrayList<SalableProduct> productList;
+    /**
+     * Constructs a new ShoppingCart object.
+     */
+    public ShoppingCart() {
+        productList = new ArrayList<SalableProduct>();
+    }
+
+    /**
+     * Adds the specified product to the shopping cart.
+     * @param product the product to be added to the shopping cart
+     */
+    public void addProduct(SalableProduct product) {
+        productList.add(product);
+    }
+
+    /**
+     * Removes the specified product from the shopping cart.
+     * @param product the product to be removed from the shopping cart
+     */
+    public void removeProduct(SalableProduct product) {
+        productList.remove(product);
+    }
+
+    /**
+     * Returns the total cost of all products in the shopping cart.
+     * @return the total cost of all products in the shopping cart
+     */
+    public double getTotal() {
+        double total = 0;
+
+        for (SalableProduct product : productList) {
+            total += product.getPrice();
+        }
+        total = (double)Math.round(total * 100) / 100;
+        return total;
+    }
+    
+    /**
+    The checkout method is used to purchase all the products in the shopping cart from a given store.
+    It loops through each product in the cart and calls its purchase() method with a quantity of 1.
+    After purchasing all the products, it clears the shopping cart.
+    @param store the store from which to purchase the products
+    */
+    public void checkout(StoreFront store) {
+	    for (SalableProduct product : productList) {
+	    store.purchaseProduct(product, 1);
+	    }
+	    
+	    productList.clear();
+    }
+    /**
+
+    The cancelOrder method is used to cancel the purchase of all the products in the shopping cart from a given store.
+    It loops through each product in the cart and calls its cancelPurchase() method with a quantity of 1.
+    After canceling the purchase of all the products, it clears the shopping cart.
+    @param store the store from which to cancel the purchase of the products
+    */
+    public void cancelOrder(StoreFront store) {
+	    for (SalableProduct product : productList) {
+	    store.cancelPurchase(product, 1);
+	    }
+	    
+	    productList.clear();
+    }
+}
+
---
+++ milestone/src/milestone/StoreFront.java
@@ -0,0 +1,71 @@
+package milestone;
+
+public class StoreFront {
+    private String name;
+    private String location;
+    private InventoryManager inventory;
+    /**
+     * Constructs a new StoreFront object with the specified name, location, and inventory manager.
+     * @param name the name of the store
+     * @param location the location of the store
+     * @param inventory the inventory manager for the store
+     */
+    public StoreFront(String name, String location, InventoryManager inventory) {
+        this.setName(name);
+        this.setLocation(location);
+        this.inventory = inventory;
+    }
+
+    /**
+     * Returns the name of the store.
+     * @return the name of the store
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name of the store.
+     * @param name the new name of the store
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the location of the store.
+     * @return the location of the store
+     */
+    public String getLocation() {
+        return location;
+    }
+
+    /**
+     * Sets the location of the store.
+     * @param location the new location of the store
+     */
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    /**
+     * Purchases the specified quantity of the specified product from the store's inventory.
+     * @param product the product to purchase
+     * @param quantity the quantity of the product to purchase
+     */
+    public void purchaseProduct(SalableProduct product, int quantity) {
+        int qty = product.getQuantity() - quantity;
+        inventory.updateProduct(product, qty);
+    }
+
+    /**
+     * Cancels a purchase of the specified quantity of the specified product, returning it to the store's inventory.
+     * @param product the product to return to inventory
+     * @param quantity the quantity of the product to return to inventory
+     */
+    public void cancelPurchase(SalableProduct product, int quantity) {
+        int qty = product.getQuantity() + quantity;
+        inventory.updateProduct(product, qty);
+    }
+}
+
